e497f873d2625622d3defc95384e4cb89b779d6a
Ankit Kumar <ankitk@stanford.edu>
2014-09-06 15:07:26 +0200

    finished PCA. still need to check if it is working, though

--- src/PCA.py
+++ src/PCA.py
@@ -10,7 +10,7 @@ import splunklib.results as results
 from collections import defaultdict
 # from scipy.stats import multivariate_normal
 import numpy as np
-from base_classes import SplunkRegressorBase
+from base_classes import SplunkProjectorBase
 import sys
 import splunkmath as sm
 from splunkmath.utils.strings import *
@@ -30,35 +30,35 @@ class SplunkPCA(SplunkProjectorBase):
 	def __init__(self, host, port, username, password):
 		super(SplunkPCA, self).__init__(host, port, username, password)
 
-	def project_splunk_search(self, search_string, X_fields, Y_field, output_field):
+	def project_splunk_search(self, search_string, X_fields, output_field):
 		'''
-		uses splunkmath to predict every event in the splunk search.
+		uses splunkmath to project every event in the splunk search.
 		'''
-		# make a bias term
-		splunk_search = 'search %s | eval bias=1' % search_string
-		# instantiate arrays for x and theta
-		x = sm.array(X_fields + ['bias']) # this is sorta hacky as well. maybe make it easier to do this with splunkmath?
-		theta = sm.array(self.theta)
+		# initialize search
+		splunk_search = 'search %s'
+		# instantiate arrays for x and components
+		x = sm.array(X_fields)
+		components = sm.array(self.components)
 		# do the dot product
-		h_x = sm.dot(theta, sm.transpose(x)) # by convention we need to tranpose x... maybe this is the wrong convention.
-		h_x.rename_elem(0,0,output_field)
-		splunk_search_string = splunk_concat(splunk_search, h_x.string)
+		projection = sm.dot(x, components)
+		# rename the vector to output_field; projected field names will be output_field_i_j
+		projection.rename(output_field)
+		splunk_search_string = splunk_concat(splunk_search, projection.string)
 		
 		return splunk_search_string
 
 
-	def project_single_event(self, event_to_predict, X_fields, Y_field):
+	def project_single_event(self, event_to_project, X_fields):
 		'''
 		uses numpy to predict a single event
 		'''
 		# this is easy to do with pdfs, but just to make sure we have everything correct, we'll do everything out
 		# turn the event into a numpy array
-		x, y = sm.event_to_numpy_reps_continuous_continuous(event_to_predict, self.feature_mapping, Y_field, bias=True)	
+		x, y = sm.event_to_numpy_reps_continuous_continuous(event_to_project, self.feature_mapping, X_fields[0])	#x_fields[0] to throw away
 
-		# h(x) defined as theta^T x
-		h_x = np.dot(self.theta.T, x)
-		
-		return h_x
+		# projection defined as x dot self.components (1x45 * 45x4 -> 1x4)
+
+		return np.dot(x, self.components)
 
 	
 
@@ -78,12 +78,11 @@ class SplunkPCA(SplunkProjectorBase):
 		self.feature_mapping =feature_mapping
 		# make the search job
 		search_kwargs = {'timeout':1000, 'exec_mode':'blocking'}
-		job = self.jobs.create('search ' +search_string + '| table %s' % ' '.join(X_fields)  **search_kwargs)
+		job = self.jobs.create('search ' +search_string + '| table %s' % ' '.join(X_fields),  **search_kwargs)
 		# note that we are just disregarding the y here because of how job_to_numpy_reps was coded. will change later.
-		X, y = sm.job_to_numpy_reps(job, feature_mapping, X_fields[0], ('continuous','continuous'), bias=True)
+		X, y = sm.job_to_numpy_reps(job, feature_mapping, X_fields[0], ('continuous','continuous')) 
 
 		# now we find the principal n eigenvectors
-
 		# first subtract the means; save it to user later
 		self.means = np.average(X, axis=0)
 		X = X - self.means
@@ -91,16 +90,24 @@ class SplunkPCA(SplunkProjectorBase):
 		# now compute covariance matrix
 		cov = np.dot(X.T, X)
 
-		# now compute n eigenvectors
-		
-		self.theta = np.dot(np.dot(np.linalg.inv(np.dot(X.T, X)), X.T), y)
-		print self.theta.shape
+		# now compute eigenvectors
+		eigval, eigvec = np.linalg.eig(cov)
+
+		# sort the eigvals
+		sorted_indices = np.argsort(eigval)[::-1][:n]
+
+		# find the right eigvecs; now the principal n eigvecs are the columns of principal_n_eigvecs
+		principal_n_eigvecs = eigvec[:,sorted_indices]
+
+		# store it in self.components
+		self.components = principal_n_eigvecs
+		print self.components.shape
 
 if __name__ == '__main__':
 	username = raw_input("What is your username? ")
 	password = raw_input("What is your password? ")
-	slr = SplunkLinearRegression(host="localhost", port=8089, username=username, password=password)
-	slr.test_accuracy_splunk_search(reaction_search, reaction_search,reaction_features, reaction_class)
+	PCA = SplunkPCA(host="localhost", port=8089, username=username, password=password)
+	PCA.train(reaction_search, reaction_features, 4)
 	
 
 
